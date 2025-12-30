cc97ab104a46cd5a91452c246f511bd11c674cf0
nxfive <kontakt@nxfive.pl>
2025-12-30 23:12:07 +0100

    feat: Update /predict and /predict_many to use prediction DI

--- services/backend/fastapi/main.py
+++ services/backend/fastapi/main.py
@@ -1,30 +1,28 @@
-import os
 from typing import Annotated
 
 from fastapi import Depends, FastAPI
-from sqlalchemy.orm import Session
 
-from database.db import get_db
-
-from .predict import PredictService
-from .schemas import MedicalCostFeatures
+from .prediction import get_prediction_service
+from .prediction_service import PredictionService
+from .schemas import (MedicalCostFeatures, PredictionManyResponse,
+                      PredictionResponse)
 
 app = FastAPI()
 
-bento_host = os.getenv("BENTO_HOST") or "127.0.0.1"
-bento_port = os.getenv("BENTO_PORT") or 3000
 
-ps = PredictService(bento_url=f"http://{bento_host}:{bento_port}")
-
-
-@app.post("/predict")
-def predict(features: MedicalCostFeatures, db: Annotated[Session, Depends(get_db)]) -> dict:
-    result = ps.predict(db, features)
+@app.post("/predict", response_model=PredictionResponse)
+def predict(
+    features: MedicalCostFeatures,
+    service: Annotated[PredictionService, Depends(get_prediction_service)],
+) -> dict[str, float]:
+    result = service.predict(features)
     return {"charges": result}
 
 
-@app.post("/predict_many")
+@app.post("/predict_many", response_model=PredictionManyResponse)
 def predict_many(
-    features_list: list[MedicalCostFeatures], db: Annotated[Session, Depends(get_db)]) -> dict:
-    result = ps.predict_many(db, features_list)
+    features_list: list[MedicalCostFeatures],
+    service: Annotated[PredictionService, Depends(get_prediction_service)],
+) -> dict[str, float]:
+    result = service.predict_many(features_list)
     return {"charges": result}
