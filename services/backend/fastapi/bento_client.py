6c41d70cf53a4ea873de5f21c4ef6612d258298a
nxfive <kontakt@nxfive.pl>
2025-12-30 22:59:31 +0100

    feat: Add global BentoClient instance with host and port config

--- services/backend/fastapi/bento_client.py
+++ services/backend/fastapi/bento_client.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 
 
@@ -17,7 +19,7 @@ class BentoClient:
 
     def predict_many(self, data_list: list[dict]) -> list[float]:
         """
-        Sends multiple input data dictionaries to the BentoML service for batch 
+        Sends multiple input data dictionaries to the BentoML service for batch
         prediction.
         """
         response = requests.post(
@@ -27,3 +29,9 @@ class BentoClient:
         )
         response.raise_for_status()
         return response.json()["charges"]
+
+
+BENTO_HOST = os.getenv("BENTO_HOST") or "127.0.0.1"
+BENTO_PORT = os.getenv("BENTO_PORT") or 3000
+
+bento_client = BentoClient(bento_url=f"http://{BENTO_HOST}:{BENTO_PORT}")
